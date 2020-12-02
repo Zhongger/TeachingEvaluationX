3f758264b0cfe5463b13b5aac9552c3d2998e928
zhongmingyi <zhongmingyi@huya.com>
2020-12-02 20:11:17 +0800

    feat:全部模块跑通

--- TEX-admin/src/main/java/com/zhongger/TEXApplication.java
+++ TEX-admin/src/main/java/com/zhongger/TEXApplication.java
@@ -8,6 +8,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class TEXApplication {
     public static void main(String[] args) {
         SpringApplication.run(com.zhongger.TEXApplication.class, args);
-        System.out.println("教学评价管理系统启动成功~");
+        System.out.println(" 教学评价管理系统启动成功~");
     }
 }
